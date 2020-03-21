7ba8ee9b205f357c6e5a8f8e89aa2688b037706a
tongtong ding <tding7@asu.edu>
2020-03-20 22:25:58 -0700

    added quotation mark

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ function generatePassword() {
   var generatePasswordSpe = confirm("Do you want special characters?");
 
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passNumber = "012345678";
-var specialCharac = "~!@#$%^&*()-_=+";
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var passNumber = ["0","1","2","3","4","5","6","7","8","9"];
+var specialCharac = ["~","!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
 
 var content = ""
 
@@ -60,12 +60,9 @@ for (var i = 0; i < generatePasswordLength; i++) {
 
 };
 
-return password
+return password;
 
 };
-
-
-
 
 
 
